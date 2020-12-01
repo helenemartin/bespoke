91fdefd32f7c4e55ea464a9cd93bd3785584f016
Your Name <helenemartin13@hotmail.com>
2020-12-01 18:12:35 +0000

    create a fixtures folder, create a feature.json file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Feature from "../components/feature";
+import { FeatureContainer } from "../containers/feature";
 
 export default function Home() {
-  return <Feature.Container />;
+  return <FeatureContainer />;
 }
