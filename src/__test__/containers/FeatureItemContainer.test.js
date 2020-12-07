c0a639041f8d6764dd2348d915e4969c90e7527b
Juan Pablo Blanco <jpblancodb@gmail.com>
2020-12-07 21:54:26 +0100

    Add unit test for FeatureItemContainer

--- src/__test__/containers/FeatureItemContainer.test.js
+++ src/__test__/containers/FeatureItemContainer.test.js
@@ -1,7 +1,19 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { FeatureItemContainer } from "../../containers/FeatureItemContainer";
 
+const mockSpeak = jest.fn();
+
+jest.mock("react-speech-kit", () => ({
+  useSpeechSynthesis: () => ({
+    speak: mockSpeak,
+  }),
+}));
+
 describe("FeatureItemContainer Test", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("should render title", () => {
     // given
     const expectedItem = {
@@ -18,8 +30,23 @@ describe("FeatureItemContainer Test", () => {
     const { container } = render(<FeatureItemContainer item={expectedItem} />);
 
     // then
-    // expect(container).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
     expect(screen.queryByTestId("title")).toBeInTheDocument();
     expect(screen.getByTestId("title").textContent).toBe(expectedItem.title);
+  });
+
+  test("should invoke speak when clicking the item", () => {
+    // given
+    const expectedItem = {
+      spokenText: "Hello world",
+    };
+    render(<FeatureItemContainer item={expectedItem} />);
+
+    // when
+    fireEvent.click(screen.getByTestId("clickable-feature"));
+
+    // then
+    expect(mockSpeak).toBeCalledTimes(1);
+    expect(mockSpeak).toBeCalledWith({ text: expectedItem.spokenText });
   });
 });
