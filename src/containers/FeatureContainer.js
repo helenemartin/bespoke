19d49363f18156e75249d19e857aadde22ef4908
Your Name <helenemartin13@hotmail.com>
2021-01-26 20:33:29 +0000

    shuffle the card image deck, to randomize on page refresh

--- src/containers/FeatureContainer.js
+++ src/containers/FeatureContainer.js
@@ -5,15 +5,17 @@ import { FeatureItemContainer } from "./FeatureItemContainer";
 import { useSpeechSynthesis } from "react-speech-kit";
 import { useMemo } from "react";
 
-function shuffleArray() {
-  const indexes = [2, 0, 1];
-  const randomArray = [
-    featureData[indexes[0]],
-    featureData[indexes[1]],
-    featureData[indexes[2]]
-  ];
-  return randomArray;
+function shuffleArray(sorting) {
+  const cardDeck = [...sorting];
+  const deal = [];
+  for (let count = 0; count < sorting.length; count += 1) {
+    deal.push(
+      cardDeck.splice(Math.floor(Math.random() * cardDeck.length), 1)[0]
+    );
+  }
+  return deal;
 }
+
 export function FeatureContainer() {
   const { speak, voices } = useSpeechSynthesis();
   console.log(voices);
@@ -22,12 +24,9 @@ export function FeatureContainer() {
     [voices]
   );
 
-  console.log(Karen);
-  const randomFeatureData = shuffleArray();
-
   return (
     <Feature.Container>
-      {randomFeatureData.map(item => (
+      {shuffleArray(featureData).map(item => (
         <FeatureItemContainer
           item={item}
           key={item.id}
